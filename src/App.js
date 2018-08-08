5b46c98931ecae6784d90462c191ce1c1c7ced42
Albino Tonnina <albinotonnina@gmail.com>
2018-08-08 01:30:14 +0100

    no message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <span>Hello</span>
+        <span>Hello World</span>
       </ThemeProvider>
     )
   }
